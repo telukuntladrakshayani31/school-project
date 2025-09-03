e436ad7ec822b8c484cca3e1279fedce9a441cae
DRAKSHAYANI <telukuntladrakshayani@gmail.com>
2025-09-03 18:54:27 +0530

    Fix: updated react-scripts to 5.0.1 in frontend

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",     // since you’re running locally
-  user: "root",          // default MySQL user
-  password: "grapes@31", // your password
-  database: "schooldb",   // we will create this DB
-  port: 3306             // default MySQL port
+  host: process.env.MYSQLHOST,     
+  user: process.env.MYSQLUSER,          
+  password: process.env.MYSQLPASSWORD, 
+  database: process.env.MYSQLDATABASE,   
+  port: process.env.MYSQLPORT             
 });
-
 
 db.connect((err) => {
   if (err) {
     console.error("❌ MySQL Connection Failed: ", err);
     return;
   }
-  console.log("✅ Connected to MySQL Database!");
+  console.log("✅ Connected to Railway MySQL Database!");
 });
 
 module.exports = db;
--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use("/schoolImages", express.static("public/schoolImages"));
 
 app.use("/api/schools", schoolRoutes);
 app.use(express.static(path.join(__dirname, "build")));
-app.get(/.*/, (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
