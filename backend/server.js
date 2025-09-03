3a3196b469de765805f8a8d9ccaaec81894d2399
DRAKSHAYANI <telukuntladrakshayani@gmail.com>
2025-09-03 16:29:13 +0530

    Fix path-to-regexp bug with regex fallback route

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const schoolRoutes = require("./routes/schoolRoutes");
+const path = require("path");
 
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
-app.use("/schoolImages", express.static("public/schoolImages")); // serve images
+app.use("/schoolImages", express.static("public/schoolImages"));
 
 
 app.use("/api/schools", schoolRoutes);
 app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
